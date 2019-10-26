5d37a267677383b793c8ab06fa73677c1c47a6d9
Nortyr <321785796@qq.com>
2019-10-26 17:43:35 +0800

    java8

---
+++ src/main/java/com/uino/javalearn/lambda/LambdaLearn2.java
@@ -0,0 +1,53 @@
+package com.uino.javalearn.lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.stream.Collectors.reducing;
+
+public class LambdaLearn2 {
+    public static void main(String []args){
+        lambdaDemo1();
+    }
+
+    private static void lambdaDemo1() {
+        Student student=new Student();
+        Student student1=new Student();
+        Student student2=new Student();
+        Student student3=new Student();
+        Student student4=new Student();
+        student.setName("张三");
+        student.setClassname("高三一");
+        student.setScored(100);
+        student.setWeight(80.1);
+        student1.setSex("男");
+        student1.setName("李四");
+        student1.setClassname("高三一");
+        student1.setScored(90);
+        student1.setWeight(60.3);
+        student1.setSex("男");
+        student2.setName("小红");
+        student2.setClassname("高三一");
+        student2.setScored(95);
+        student2.setWeight(50);
+        student2.setSex("女");
+        student3.setName("王五");
+        student3.setClassname("高三二");
+        student3.setScored(100);
+        student3.setWeight(80.1);
+        student3.setSex("男");
+        student4.setName("赵六");
+        student4.setClassname("高三三");
+        student4.setScored(100);
+        student4.setWeight(80.1);
+        student4.setSex("男");
+        List<Student> list =new ArrayList<>();
+        list.add(student);
+        list.add(student1);
+        list.add(student2);
+        list.add(student3);
+        list.add(student4);
+        /////////////////
+        int su=list.stream().collect(reducing(0,Student::getScored,Integer::sum));
+    }
+}
---
+++ src/main/java/com/uino/javalearn/lambda/Student.java
@@ -0,0 +1,12 @@
+package com.uino.javalearn.lambda;
+
+import lombok.Data;
+
+@Data
+public class Student {
+    private String name;
+    private String classname;
+    private double weight;
+    private int scored;
+    private String sex;
+}
