83fa6cd6eb1e4558ef3740c75f509e3040e8e2b8
chenhui <chenhui@uinnova.com>
2019-11-04 18:04:48 +0800

    feat:2019/11/4

---
+++ src/main/java/com/uino/javalearn/lambda/StudentEasy.java
@@ -0,0 +1,12 @@
+package com.uino.javalearn.lambda;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class StudentEasy {
+    private String name;
+    private String sex;
+
+}
