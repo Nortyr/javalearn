ee11defe6d618baa3f08a1f9e4b0d3c068144648
chenhui <chenhui@uinnova.com>
2019-12-03 09:19:20 +0800

    feat:2019/12/2 线程创建2

--- src/main/java/com/uino/javalearn/concurrent/test2/CreateThread.java
+++ src/main/java/com/uino/javalearn/concurrent/test2/CreateThread.java
@@ -1,10 +1,34 @@
 package com.uino.javalearn.concurrent.test2;
 
 public class CreateThread {
+    /**
+     *计数器
+     */
+    private static int num=0;
     public static void main(String[] args) {
-        testConcurrent2();
+        testConcurrent3();
     }
-    
+
+    private static void testConcurrent4() {
+        for (int i = 0; i < 10; i++) {
+            new Thread(()->{
+                for (int j = 0; j < 100; j++) {
+                    inCreate();
+                }
+            }).start();
+        }
+        System.out.println(num);
+
+    }
+
+    private static void testConcurrent3() {
+        Runnable r=new MyThread3();
+        Thread t=new Thread(r);
+        Thread t1=new Thread(r);
+        t.start();
+        t1.start();
+    }
+
     /**
     * @author 陈晖
     * @description :这下2个线程的run锁的是一个对象了，只有一个线程能循环
@@ -35,7 +59,7 @@ public class CreateThread {
 
     /**
     * @author 陈晖
-    * @description :测试继承Thread类
+    * @description :2个线程没有访问共同的，所以没乱
     * @date 15:59 2019/12/2
     * @return void
     */
@@ -44,5 +68,11 @@ public class CreateThread {
         Thread myThread1=new MyThread();
         myThread.start();
         myThread1.start();
+    }
+    /**
+     *静态方法，直接对num计数器进行++操作。
+     */
+    public static void inCreate(){
+        num++;
     }
 }
---
+++ src/main/java/com/uino/javalearn/concurrent/test2/MyThread3.java
@@ -0,0 +1,14 @@
+package com.uino.javalearn.concurrent.test2;
+
+public class MyThread3 implements Runnable {
+    public static int sum;
+
+    public void run() {
+
+        for(int i=0;i<1000;i++){
+            sum+=i;
+
+        }
+        System.out.println(sum);
+    }
+}
